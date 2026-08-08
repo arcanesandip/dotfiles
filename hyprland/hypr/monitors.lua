3040f44e29bf50edac2e2af21884e9e7f7a8a3ab
sandip <136832190+arcanesandip@users.noreply.github.com>
2026-08-08 07:16:28 +0545

    feat: add modular hyprland lua config and custom updates

---
+++ hyprland/hypr/monitors.lua
@@ -0,0 +1,13 @@
+------------------
+---- MONITORS ----
+------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+hl.monitor({
+	output = "eDP-1",
+	mode = "1920x1080@120",
+	position = "auto",
+	scale = 1.30,
+	cm = "hdr",
+	bitdepth = 8
+})
---
+++ hyprland/hypr/myPrograms.lua
@@ -0,0 +1,7 @@
+---------------------
+---- MY PROGRAMS ----
+---------------------
+-- Set programs that you use
+terminal = "kitty"
+fileManager = "nautilus"
+menu = "hyprlauncher"
